928e9373203c2ecbc7cc7187f9fcbc1d126a056d
lolstudio <lolstudio@users.noreply.github.com>
2026-09-04 18:17:45 +0800

    Update MainActivity.java

--- app/src/main/java/com/lolstudio/lizhimusic/MainActivity.java
+++ app/src/main/java/com/lolstudio/lizhimusic/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
